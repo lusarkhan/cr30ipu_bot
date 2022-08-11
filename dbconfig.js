e3d07d48187b14f95a3fb0dd8fe270cf24f10130
Rasul T <108508716+lusarkhan@users.noreply.github.com>
2022-08-11 13:56:17 +0400

    add change yml

--- dbconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user          : process.env.NODE_ORACLEDB_USER || "SARV_PAY",
-    password      : process.env.NODE_ORACLEDB_PASSWORD || "pay",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.3.2.104/pay",
-    externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-};
