79807a1d1bccafe134e15303b391540cabc9b9a4
LusarKhan <108508716+lusarkhan@users.noreply.github.com>
2024-05-12 21:49:15 +0400

    Update dbconfig.js

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: process.env.NODE_ORACLEDB_USER || "SARV_PAY",
-    password: process.env.NODE_ORACLEDB_PASSWORD || "pay",
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.3.2.104/pay",
+    user: process.env.NODE_ORACLEDB_USER || "USER",
+    password: process.env.NODE_ORACLEDB_PASSWORD || "password",
+    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.0.0.1/db",
     externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
 };
